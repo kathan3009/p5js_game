5f973f833aed79c7653e634419abefb1cd5b1ee3
kathan3009 <kthndesai@gmail.com>
2020-12-26 02:42:05 +0530

    first commit

---
+++ sketch.js
@@ -0,0 +1,59 @@
+let rand;
+let randAngle;
+let funRand
+
+function setup() {
+
+  createCanvas(windowWidth, windowHeight, WEBGL);
+  angleMode(DEGREES);
+  rand = int(random(1, 10) * 10)
+  randAngle = int(random(2, 20) * 10)
+  funRand = int(random(0, 2))
+
+
+}
+
+function draw() {
+  background(0);
+  if (funRand == 0) {
+    pattern()
+  } else {
+    design()
+  }
+
+
+
+}
+
+function pattern() {
+  rotateZ(randAngle);
+  for (let i = 0; i < 400; i++) {
+    let r = random(60);
+    rotate(frameCount / 50)
+    stroke(r * 8);
+    line(20, i, 20 + r, i);
+  }
+}
+
+function design() {
+
+  rotateX(randAngle);
+
+  noFill()
+  stroke(255)
+  for (var i = 0; i < 50; i++) {
+    stroke(231, 41, 124)
+    rotate(frameCount / 80)
+    beginShape()
+    for (var j = 0; j < 360; j += rand) {
+      var rad = i * 3;
+      var x = rad * cos(j);
+      var y = rad * sin(j);
+      var z = sin(frameCount * 2 + i * 5) * 50
+      vertex(x, y, z)
+    }
+
+    endShape(CLOSE);
+  }
+
+}
